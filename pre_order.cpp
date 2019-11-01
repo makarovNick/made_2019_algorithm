377006e5ceace8d3470476aee566c3ecb773eff4
makarovNick <polucia112233@gmail.com>
2019-11-02 00:24:43 +0300

    first commit

---
+++ pre_order.cpp
@@ -0,0 +1,105 @@
+/*************************************************************************************/
+//6_1.Обход дерева в порядке pre - order
+//Ограничение времени	0.2 секунды
+//Ограничение памяти	64Mb
+//Ввод	стандартный ввод или input.txt
+//Вывод	стандартный вывод или output.txt
+//Дано число N < 106 и последовательность целых чисел из[-231..231] длиной N.
+//Требуется построить бинарное дерево, заданное наивным порядком вставки.Т.е., 
+//при добавлении очередного числа K в дерево с корнем root, если root→Key ≤ K, 
+//то узел K добавляется в правое поддерево root; иначе в левое поддерево root.
+//Выведите элементы в порядке pre - order(сверху вниз).
+//
+//Рекурсия запрещена.
+/*************************************************************************************/
+#include <iostream>
+#include <stack>
+#include <functional>
+
+class tree
+{
+	using function = std::function<void(const int)>;
+	struct node
+	{
+		int value;
+		node* left;
+		node* right;
+
+		node(int v) : value(v), left(nullptr), right(nullptr) {}
+	};
+public:
+	tree() : root(nullptr) {}
+	void insert(const int val);
+	void preOrderTraversal(const function& func) const;
+private:
+	node* root;
+	void insert(const int val, node* node);
+};
+
+
+void tree::insert(const int val) {
+	if (root == nullptr) 
+	{
+		root = new node(val);
+	}
+	else 
+	{
+		insert(val, root);
+	}
+}
+
+void tree::insert(const int val, node* node)
+{
+	tree::node* newnode = new tree::node(val);
+
+	tree::node* x = root;
+	tree::node* y = nullptr;
+
+	while (x != nullptr) {
+		y = x;
+		if (val < x->value)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	if (y == nullptr)
+		y = newnode;
+	else if (val < y->value)
+		y->left = newnode;
+	else
+		y->right = newnode;
+}
+void tree::preOrderTraversal(const function& func) const
+{ 
+	if (root == nullptr)
+		return;
+
+	std::stack<node*> nodeStack;
+	nodeStack.push(root);
+
+	while (nodeStack.empty() == false)
+	{
+		struct node* node = nodeStack.top();
+		func(node->value);
+		nodeStack.pop();
+
+		if (node->right)
+			nodeStack.push(node->right);
+		if (node->left)
+			nodeStack.push(node->left);
+	}
+}
+int main()
+{
+	tree tr;
+	size_t n;
+	int v;
+	std::cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cin >> v;
+		tr.insert(v);
+	}
+	tr.preOrderTraversal([](const int v) {std::cout << v << " "; });
+	return 0;
+}
---
+++ treap.cpp
@@ -0,0 +1,365 @@
+/********************************************************************************************/
+//7_1.Солдаты ДД
+//Ограничение времени	1 секунда
+//Ограничение памяти	500Mb
+//Ввод	стандартный ввод или input.txt
+//Вывод	стандартный вывод или output.txt
+//В одной военной части решили построить в одну шеренгу по росту.Т.к.часть была далеко не образцовая, то солдаты часто приходили не вовремя, 
+//а то их и вовсе приходилось выгонять из шеренги за плохо начищенные сапоги.
+//Однако солдаты в процессе прихода и ухода должны были всегда быть выстроены по росту – сначала самые высокие, 
+//а в конце – самые низкие.За расстановку солдат отвечал прапорщик, который заметил интересную особенность – все солдаты в части разного роста.
+//
+//Ваша задача состоит в том, чтобы помочь прапорщику правильно расставлять солдат, а именно для каждого приходящего солдата указывать, 
+//перед каким солдатом в строе он должен становится.Требуемая скорость выполнения команды - O(log n) в среднем.
+//
+//В реализации используйте декартовы деревья.
+//
+//Формат ввода
+//Первая строка содержит число N – количество команд(1 ≤ N ≤ 90 000).
+//
+//В каждой следующей строке содержится описание команды : число 1 и X если 
+//солдат приходит в строй(X – рост солдата, натуральное число до 100 000 включительно) и число 2 и Y если солдата, 
+//стоящим в строе на месте Y надо удалить из строя.Солдаты в строе нумеруются с нуля.
+//
+//Формат вывода
+//На каждую команду 1 (добавление в строй) вы должны выводить число K – номер позиции, на которую должен встать этот солдат(все стоящие за ним двигаются назад).
+/********************************************************************************************/
+#include <iostream>
+
+template <typename T>
+class treap
+{
+
+	struct node
+	{
+	public:
+		T value;
+		unsigned priority;
+		int tree_size = 1;
+		node* left = nullptr;
+		node* right = nullptr;
+
+		int size();
+		int sizeOfLeft()
+		{
+			return (left != nullptr) ? left->tree_size : 0;
+		}
+
+		node(T init_key, unsigned init_priority)
+			: value(init_key)
+			, priority(init_priority)
+		{
+		}
+		node(T init_key)
+			: node(init_key, getPriority())
+		{
+		}
+		~node()
+		{
+			delete left;
+			delete right;
+		}
+
+	private:
+		unsigned getPriority();
+	};
+
+public:
+	bool insert(T val);
+	bool contains(T val);
+	bool erase(T val);
+	int getPosition(T val);
+	T findByPosition(int keyPos)
+	{
+		return findByPosition(root, keyPos + 1);
+	}
+	int size()
+	{
+		return (root != nullptr) ? root->tree_size : 0;
+	}
+
+	treap()
+		: root(nullptr) {};
+	~treap()
+	{
+		delete root;
+	};
+
+private:
+	node* root;
+	// implements split + merge
+	node* rotateRight(node* x);
+	// implements split + merge
+	node* rotateLeft(node* y);
+	node* _insert(node* curNode, T val);
+	// finds position of value if treap contains it
+	// with each step on tree change value of keyPos
+	// returns node
+	node* _find(node* curNode, T val, int& keyPos);
+	// split + delete + merge
+	node* _erase(node* curNode, T val, bool& erased);
+	T findByPosition(node* curNode, int keyPos);
+};
+
+
+template <typename T>
+unsigned treap<T>::node::getPriority()
+{
+	//static std::mt19937 engine{ std::random_device{}() };
+	//static std::uniform_int_distribution<unsigned> distribution{
+	//	std::numeric_limits<unsigned>::min(), std::numeric_limits<unsigned>::max()
+	//};
+	//return distribution(engine);
+
+	return rand() ^ (rand() << 15);
+}
+
+template <typename T>
+inline int treap<T>::node::size()
+{
+	int left_size = (left != nullptr) ? left->tree_size : 0,
+		right_size = (right != nullptr) ? right->tree_size : 0;
+
+	return tree_size = left_size + 1 + right_size;
+}
+
+template <typename T>
+typename treap<T>::node* treap<T>::rotateRight(node* x)
+{
+	node* y = x->left;
+	node* T2 = y->right;
+
+	y->right = x;
+	x->left = T2;
+
+	x->size();
+	y->size();
+
+	return y;
+}
+
+template <typename T>
+typename treap<T>::node* treap<T>::rotateLeft(node* y)
+{
+	node* x = y->right;
+	node* T2 = x->left;
+
+	x->left = y;
+	y->right = T2;
+
+	y->size();
+	x->size();
+
+	return x;
+}
+
+template <typename T>
+typename treap<T>::node* treap<T>::_find(node* curNode, T value, int& keyPos)
+{
+	if (curNode == nullptr)
+		return nullptr;
+
+	node* answer = nullptr;
+	int rootPos = 1 + curNode->sizeOfLeft();
+
+	if (value == curNode->value)
+	{
+		keyPos += rootPos;
+		answer = curNode;
+	}
+	else if (value > curNode->value)
+	{
+		answer = _find(curNode->left, value, keyPos);
+	}
+	else if (value < curNode->value)
+	{
+		keyPos += rootPos;
+		answer = _find(curNode->right, value, keyPos);
+	}
+
+	return answer;
+}
+
+template <typename T>
+inline bool treap<T>::contains(T value)
+{
+	int keyPos = 0;
+
+	if (_find(root, value, keyPos) != nullptr)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+template <typename T>
+inline int treap<T>::getPosition(T value)
+{
+	int keyPos = 0;
+	if (_find(root, value, keyPos) != nullptr)
+	{
+		return keyPos - 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+template <typename T>
+inline bool treap<T>::erase(T value)
+{
+	bool erased = false;
+	root = _erase(root, value, erased);
+
+	return erased;
+}
+
+template <typename T>
+T treap<T>::findByPosition(node* curNode, int keyPos)
+{
+
+	int rootPos = 1 + curNode->sizeOfLeft();
+
+	if (keyPos == rootPos)
+	{
+		return curNode->value;
+	}
+	else if (keyPos < rootPos)
+	{
+		return findByPosition(curNode->left, keyPos);
+	}
+	else
+	{
+		return findByPosition(curNode->right, keyPos - rootPos);
+	}
+}
+
+template <typename T>
+typename treap<T>::node* treap<T>::_insert(node* curNode, T value)
+{
+	if (curNode == nullptr)
+		return new node(value);
+
+	if (value > curNode->value)
+	{
+		curNode->left = _insert(curNode->left, value);
+
+		if (curNode->left->priority > curNode->priority)
+			curNode = rotateRight(curNode);
+	}
+	else
+	{
+		curNode->right = _insert(curNode->right, value);
+
+		if (curNode->right->priority > curNode->priority)
+			curNode = rotateLeft(curNode);
+	}
+
+	curNode->size();
+
+	return curNode;
+}
+
+template <typename T>
+inline bool treap<T>::insert(T value)
+{
+	if (contains(value))
+	{
+		return false;
+	}
+	else
+	{
+		root = _insert(root, value);
+		return true;
+	}
+}
+
+template <typename T>
+typename treap<T>::node* treap<T>::_erase(node* curNode, T value, bool& erased)
+{
+	if (curNode == nullptr)
+		return nullptr;
+
+	node* left = curNode->left;
+	node* right = curNode->right;
+
+	if (value > curNode->value)
+	{
+		curNode->left = _erase(left, value, erased);
+	}
+	else if (value < curNode->value)
+	{
+		curNode->right = _erase(right, value, erased);
+	}
+
+	else if (left == nullptr && right == nullptr)
+	{
+		delete curNode;
+		erased = true;
+		curNode = nullptr;
+	}
+	else if (left == nullptr)
+	{
+		curNode->right = nullptr;
+		delete curNode;
+		erased = true;
+		curNode = right;
+	}
+	else if (right == nullptr)
+	{
+		curNode->left = nullptr;
+		delete curNode;
+		erased = true;
+		curNode = left;
+	}
+
+	else if (left->priority > right->priority)
+	{
+		curNode = rotateRight(curNode);
+		curNode->right = _erase(curNode->right, value, erased);
+	}
+	else
+	{
+		curNode = rotateLeft(curNode);
+		curNode->left = _erase(curNode->left, value, erased);
+	}
+
+	if (curNode != nullptr)
+		curNode->size();
+
+	return curNode;
+}
+
+
+int main()
+{
+	treap<int> tr;
+
+	size_t n;
+	std::cin >> n;
+
+	int c;
+	int v;
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cin >> c >> v;
+		switch (c)
+		{
+		case 1:
+			tr.insert(v);
+			std::cout << tr.getPosition(v)<<std::endl;
+			break;
+		case 2:
+			tr.erase(tr.findByPosition(v));
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
