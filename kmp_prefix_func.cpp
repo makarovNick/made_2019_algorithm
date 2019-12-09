f178dc37960fa72a0b3a124bfba3cf44b1071ca2
makarovNick <56365807+makarovNick@users.noreply.github.com>
2019-12-09 21:37:21 +0300

    Update kmp_prefix_func.cpp

--- kmp_prefix_func.cpp
+++ kmp_prefix_func.cpp
@@ -2,55 +2,56 @@
 #include <string>
 #include <vector>
 
-std::vector<int> KMP(const std::string& string, const std::string& pattern) 
+std::vector<int> KMP(const std::string& string, const std::string& pattern)
 {
-	std::vector<int> prefix_function(pattern.length(), 0);
+    std::vector<int> prefix_function(pattern.length(), 0);
     std::vector<int> result;
 
-	for (int k = 0, i = 1; i < pattern.length(); ++i) 
-	{
-		while (k > 0 && pattern[i] != pattern[k])
+    for (int k = 0, i = 1; i < pattern.length(); ++i)
+    {
+        while (k > 0 && pattern[i] != pattern[k])
         {
-			k = prefix_function[k-1];
+            k = prefix_function[k - 1];
         }
 
-		if (pattern[i] == pattern[k])
+        if (pattern[i] == pattern[k])
         {
             k++;
         }
 
-		prefix_function[i] = k;
-	}
+        prefix_function[i] = k;
+    }
 
-	for (int k = 0, i = 0; i < string.length(); ++i) 
-	{
-		while (k > 0 && pattern[k] != string[i])
+    for (int k = 0, i = 0; i < string.length(); ++i)
+    {
+        while (k > 0 && pattern[k] != string[i])
         {
-            k = prefix_function[k-1];
+            k = prefix_function[k - 1];
         }
 
-		if (pattern[k] == string[i])
+        if (pattern[k] == string[i])
         {
-			k++;
+            k++;
         }
 
-		if (k == pattern.length())
+        if (k == pattern.length())
         {
             result.push_back(i - pattern.length() + 1);
         }
-	}
+    }
 
-	return result;
+    return result;
 }
+
 int main()
 {
     std::string pattern, string;
     std::cin >> pattern >> string;
-    
+
     auto indexes = KMP(string, pattern);
-    for(auto i : indexes)
+    for (auto i : indexes)
     {
-        std::cout<< i << " ";
+        std::cout << i << " ";
     }
 
     return 0;
