0263b5f751705f6bbe82722ded5af49b88663697
makarovNick <56365807+makarovNick@users.noreply.github.com>
2019-12-09 16:51:45 +0300

    array to mask
    perfomance improve x100

--- 15puzzle.cpp
+++ 15puzzle.cpp
@@ -1,396 +1,284 @@
-#define DEBUG
-
-#include <iostream>
+#include <unordered_set>
 #include <unordered_map>
-#include <queue>
-#include <algorithm>
+#include     <algorithm>
+#include      <iostream>
+#include        <vector>
+#include         <queue>
 
-const short N = 4;
-const short SOLVED[N*N] = {1,  2,  3,  4
-						, 5,  6,  7,  8
-						, 9,  10, 11, 12
-						, 13, 14, 15, 0};
+constexpr uint_fast32_t N = 4;
 
-class State 
+constexpr uint_fast32_t SOLVED = 0x123456789ABCDEF0;
+
+constexpr uint_fast32_t GET_MASK[N * N]
+    = { 0xF000000000000000, 0x0F00000000000000, 0x00F0000000000000, 0x000F000000000000,
+        0x0000F00000000000, 0x00000F0000000000, 0x000000F000000000, 0x0000000F00000000,
+        0x00000000F0000000, 0x000000000F000000, 0x0000000000F00000, 0x00000000000F0000,
+        0x000000000000F000, 0x0000000000000F00, 0x00000000000000F0, 0x000000000000000F };
+
+constexpr uint_fast32_t SET_MASK[N * N]
+    = { 0x0FFFFFFFFFFFFFFF, 0xF0FFFFFFFFFFFFFF, 0xFF0FFFFFFFFFFFFF, 0xFFF0FFFFFFFFFFFF,
+        0xFFFF0FFFFFFFFFFF, 0xFFFFF0FFFFFFFFFF, 0xFFFFFF0FFFFFFFFF, 0xFFFFFFF0FFFFFFFF,
+        0xFFFFFFFF0FFFFFFF, 0xFFFFFFFFF0FFFFFF, 0xFFFFFFFFFF0FFFFF, 0xFFFFFFFFFFF0FFFF,
+        0xFFFFFFFFFFFF0FFF, 0xFFFFFFFFFFFFF0FF, 0xFFFFFFFFFFFFFF0F, 0xFFFFFFFFFFFFFFF0 };
+
+class Solver
 {
+    struct State
+    {
+        uint_fast32_t field;
+
+        uint16_t heuristic;
+        uint16_t move_to;
+
+        State(uint_fast32_t f, uint16_t _priority, uint16_t moves)
+            : field(f)
+            , heuristic(_priority)
+            , move_to(moves)
+        {
+        }
+    };
+
 public:
-    State(const short f[N*N]) 
-	{
-        for(size_t i = 0; i < N*N; i++ )
-		{
-			field[i] = f[i];
-            if( field[i] == 0 )
-			{
-                zero_pos = i;
-			}
-		}
-    }
-    State(const State& state) 
-		: zero_pos(state.zero_pos)
-	{
-		for(size_t i = 0; i < N*N; i++ )
-		{
-			field[i] = state.field[i];
-		}
-    }
+    void        setTile(uint16_t pos, uint16_t value);
+    std::string Solve();
 
-    bool isSolved() const 
-	{
-		for (size_t i = 0; i < N*N; i++)
-		{
-			if (field[i] != SOLVED[i])
-			{
-				return false;
-			}
-		}
-
-		return true;
-    }
-
-    State moveLeft() const 
-	{
-        State new_state(*this);
-        std::swap(new_state.field[zero_pos], new_state.field[zero_pos + 1]);
-        new_state.zero_pos++;
-        return new_state;
-    }
-    State moveRight() const 
-	{
-        State new_state(*this);
-        std::swap(new_state.field[zero_pos], new_state.field[zero_pos - 1]);
-        new_state.zero_pos--;
-        return new_state;
-    }
-    State moveUp() const 
-	{
-        State new_state(*this);
-        std::swap(new_state.field[zero_pos], new_state.field[zero_pos + 4]);
-        new_state.zero_pos += 4;
-        return new_state;
-    }
-    State moveDown() const 
-	{
-        State new_state(*this);
-        std::swap(new_state.field[zero_pos], new_state.field[zero_pos - 4]);
-        new_state.zero_pos -= 4;
-        return new_state;
-    }
-
-    bool operator==(const State& state) const 
-	{
-        for (size_t i = 0; i < N*N; i++)
-		{
-			if (field[i] != state.field[i])
-			{
-				return false;
-			}
-		}
-
-		return true;
-    }
-
-    friend struct Hasher;
-
-	short getZeroPos() const
-	{
-		return zero_pos;
-	}
-	short getHeuristic() const
-	{
-		return manhattan();
-	}
-
-	auto getField() const
-	{
-		return field;
-	}
 private:
+    void        makeMove(uint_fast32_t& field, uint16_t pos);
 
-	short linear() const
+    uint16_t    getTile(uint_fast32_t field, uint16_t pos) const;
+    uint16_t    getZeroPos(uint_fast32_t field);
+    auto        getMoves(uint_fast32_t field);
+    uint16_t    getHeuristic(uint_fast32_t field);
+
+    void        changeTile(uint_fast32_t& field, uint16_t pos, uint16_t value);
+
+    bool        isSolvable() const;
+
+    uint_fast32_t start_field = 0;
+
+    struct comp
     {
-		auto conflicts = 0;
-
-    {
-        bool in_col[N*N * N*N] = { };
-        bool in_row[N*N * N*N] = { }; 
-
-        for (auto y = 0; y != N*N; ++y)
+        bool operator()(const State& a, const State& b)
         {
-            for (auto x = 0; x != N*N; ++x)
-            {
-                auto i = y * N*N + x;
-
-                auto bx = field[i] % N*N;
-                auto by = field[i] / N*N;
-
-                in_col[i] = (bx == x);
-                in_row[i] = (by == y);
-            }
+            return b.heuristic < a.heuristic;
         }
-
-        for (auto y = 0; y != N*N; ++y)
-        {
-            for (auto x = 0; x != N*N; ++x)
-            {
-                auto i = y * N*N + x;
-
-                if (field[i] == 0) // ignore the empty space
-                    continue;
-
-                if (in_col[i])
-                {
-                    for (auto r = y; r != N*N; ++r) // check down for conflicts
-                    {
-                        auto j = r * N*N + x;
-
-                        if (field[j] == 0) // ignore the empty space
-                            continue;
-
-                        if (in_col[j] && field[j] < field[i])
-                            ++conflicts;
-                    }
-                }
-
-                if (in_row[i])
-                {
-                    for (auto c = x; c != N*N; ++c) // check right for conflicts
-                    {
-                        auto j = y * N*N + c;
-
-                        if (field[j] == 0) // ignore the empty space
-                            continue;
-
-                        if (in_row[j] && field[j] < field[i])
-                            ++conflicts;
-                    }
-                }
-            }
-        }
-    }
-
-    return 2 * conflicts;
-    }
-
-	short hamming() const
-	{
-		short count = 0;
-		for (size_t i = 0; i < N*N; i++)
-		{
-			if (field[i] != SOLVED[i])
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-
-    short manhattan() const 
-	{
-		int sum = 0;
-        int distance = 0;
-        for (int i = 0; i < N*N; i++) 
-		{
-            if (field[i] != 0) 
-			{
-                distance = abs(field[i] - i - 1);
-            } 
-			else 
-			{
-                distance = N*N - i;
-            }
-            sum += distance / N;
-            distance %= N;
-            if (distance == N-1	) 
-			{
-                sum += 2;
-            } 
-			else 
-			{
-                sum += distance;
-            }
-        }
-
-        return sum;
-    }
-
-    short field[N*N];
-	
-    short zero_pos;
+    };
 };
 
-struct Hasher 
+void Solver::makeMove(uint_fast32_t& field, uint16_t pos)
 {
-    size_t operator()(const State& state) const
+    changeTile(field, getZeroPos(field), getTile(field, pos));
+    changeTile(field, pos, 0);
+}
+
+uint16_t Solver::getZeroPos(uint_fast32_t field)
+{
+    for (uint16_t i = 0; i < N * N; ++i)
     {
-        size_t hash = 0;
-        for (size_t i = 0; i < N*N; i++)
-		{
-            hash = (hash * 31 + i);
+        if (getTile(field, i) == 0)
+        {
+            return i;
+        }
+    }
+}
+
+auto Solver::getMoves(uint_fast32_t field)
+{
+    std::vector<std::pair<uint_fast32_t, char> > possible_moves;
+
+    if (getZeroPos(field) > N - 1)
+    {
+        uint_fast32_t temp = field;
+        makeMove(temp, getZeroPos(field) - N);
+
+        possible_moves.push_back(std::make_pair(temp, 'D'));
+    }
+
+    if (getZeroPos(field) < N * (N - 1))
+    {
+        uint_fast32_t temp = field;
+        makeMove(temp, getZeroPos(field) + N);
+
+        possible_moves.push_back(std::make_pair(temp, 'U'));
+    }
+
+    if (getZeroPos(field) % N != 0)
+    {
+        uint_fast32_t temp = field;
+        makeMove(temp, getZeroPos(field) - 1);
+
+        possible_moves.push_back(std::make_pair(temp, 'R'));
+    }
+
+    if (getZeroPos(field) % N != N - 1)
+    {
+        uint_fast32_t temp = field;
+        makeMove(temp, getZeroPos(field) + 1);
+
+        possible_moves.push_back(std::make_pair(temp, 'L'));
+    }
+
+    return possible_moves;
+}
+
+void Solver::changeTile(uint_fast32_t& field, uint16_t pos, uint16_t value)
+{
+    field = (field & SET_MASK[pos]) | static_cast<uint_fast32_t>(value) << (N * (N * N - pos - 1));
+}
+
+void Solver::setTile(uint16_t pos, uint16_t value)
+{
+    changeTile(start_field, pos, value);
+}
+
+uint16_t Solver::getTile(const uint_fast32_t field, uint16_t pos) const
+{
+    return (field & GET_MASK[pos]) >> (N * (N * N - 1 - pos));
+}
+
+uint16_t Solver::getHeuristic(uint_fast32_t field)
+{
+    uint16_t h = 0;
+
+    for (uint16_t i = 0; i < N * N; ++i)
+    {
+        uint16_t temp = getTile(field, i);
+
+        if (temp == 0)
+        {
+            continue;
         }
 
-        return hash;
+        h += abs((temp - 1) / N - i / N) + abs((temp - 1) % N - i % N);
+
+        if (i / N == (temp - 1) / N)
+        {
+            h += std::abs(temp - 1 - i);
+        }
     }
-};
 
-struct comparator
+    return h;
+}
+
+bool Solver::isSolvable() const
 {
-	bool operator()(const State &a, const State &b) 
-	{
-        return a.getHeuristic() >= b.getHeuristic();
+
+    uint16_t inv = 0;
+    for (uint16_t i = 0; i < N * N; ++i)
+    {
+        for (uint16_t j = i + 1; j < N * N; ++j)
+        {
+            uint_fast32_t a = getTile(start_field, i);
+            uint_fast32_t b = getTile(start_field, j);
+
+            if (a == 0 || b == 0)
+            {
+                continue;
+            }
+            if (a > b)
+            {
+                inv++;
+            }
+        }
+
+        if (getTile(start_field, i) == 0)
+        {
+            inv += 1 + i / N;
+        }
     }
-};
 
-std::string getSolution(const short field[N*N]) 
+    return inv % 2 == 0;
+}
+
+std::string Solver::Solve()
 {
-    std::priority_queue<State, std::vector<State>, comparator> queue;
-    std::unordered_map<State, std::string, Hasher> visited;
-	std::unordered_map<std::string, int> path_lenght;
+    if (!isSolvable())
+    {
+        return "-1";
+    }
 
-    State init_state(field);
-    visited[init_state];
+    std::unordered_map<uint_fast32_t, std::pair<uint_fast32_t, char> > previos_states;
+    std::priority_queue<State, std::vector<State>, comp> queue;
+    std::unordered_set<uint_fast32_t> visited;
 
-    queue.push(init_state);
+    std::string path;
 
-    State solved(SOLVED);
+    queue.push(State(start_field, 0, 0));
+    previos_states[start_field] = std::make_pair(-1, 'D');
 
-    while (!(queue.top() == solved)) 
-	{
+    while (!queue.empty())
+    {
         State current_state = queue.top();
         queue.pop();
 
-		#ifdef DEBUG
-		auto f = current_state.getField();
-		std::cout<<std::endl;
-		std::cout<<"Heuristic : "  << current_state.getHeuristic()
-			   << " Queue deepth : " << queue.size()
-			   << " Zero pos : " << current_state.getZeroPos()/N + 1<< " " <<current_state.getZeroPos()%N + 1 << std::endl;
-		for (size_t i = 0; i < N*N; i++)
-		{
-			if(i%4==0)
-				std::cout<<std::endl;
-			std::cout<<f[i]<<" ";
-		}
-		#endif
-		
-		if (queue.size() > 160) 
-		{
-            std::priority_queue<State, std::vector<State>, comparator> new_queue;
-            for (int i = 0; i < 80; ++i) 
-			{
-                new_queue.emplace(queue.top());
+        if (queue.size() > 400)
+        {
+            std::priority_queue<State, std::vector<State>, comp> new_queue;
+            for (uint16_t i = 0; i < 200; ++i)
+            {
+                new_queue.push(queue.top());
                 queue.pop();
             }
 
             queue = new_queue;
         }
 
-        if(current_state.getZeroPos() % N < N - 1) 
-		{
-            State new_state = current_state.moveLeft();
-            if(visited.find(new_state) == visited.end()) 
-			{
-                queue.emplace(new_state);
-                visited[new_state] = visited[current_state] + "L";
-            }
+        if (current_state.field == SOLVED)
+        {
+            break;
         }
-        if(current_state.getZeroPos() % N > 0) 
-		{
-            State new_state = current_state.moveRight();
-            if(visited.find(new_state) == visited.end()) 
-			{
-                queue.emplace(new_state);
-                visited[new_state] = visited[current_state] + "R";
-            }
+
+        if (visited.find(current_state.field) != visited.end())
+        {
+            continue;
         }
-        if(current_state.getZeroPos() / N < N-1) 
-		{
-            State new_state = current_state.moveUp();
-            if( visited.find(new_state) == visited.end() ) 
-			{
-                queue.emplace(new_state);
-                visited[new_state] = visited[current_state] + "U";
+
+        auto possible_moves = getMoves(current_state.field);
+
+        for (auto move : possible_moves)
+        {
+            if (visited.find(move.first) != visited.end())
+            {
+                continue;
             }
+            queue.push(State(move.first, 1 + getHeuristic(move.first), current_state.move_to + 1));
+
+            previos_states[move.first] = std::make_pair(current_state.field, move.second);
         }
-        if(current_state.getZeroPos() / N > 0) 
-		{
-            State new_state = current_state.moveDown();
-            if( visited.find(new_state) == visited.end() ) 
-			{
-                queue.emplace(new_state);
-                visited[new_state] = visited[current_state] + "D";
-            }
-        }
+
+        visited.insert(current_state.field);
     }
 
-    return visited[solved];
-}
-
-
-int getInvCount(short arr[])
-{
-	int inv_count = 0;
-	for (int i = 0; i < N * N - 1; i++)
-	{
-		for (int j = i + 1; j < N * N; j++)
-		{
-			if (arr[j] != 0
-			 && arr[i] != 0
-			 && arr[i] > arr[j])
-			 {
-				inv_count++;
-			 }
-		}
-	}
-	return inv_count;
-}
-
-int findXPosition(short puzzle[N*N])
-{
-	for (size_t i = 0; i < N*N; i++)
-	{
-		if(puzzle[i] == 0)
-		{
-			return N - (i / N);
-		}
-	}
-}
-
-bool isSolvable(short puzzle[N*N])
-{
-	int invCount = getInvCount(puzzle);
-
-	if (N & 1)
-	{
-		return !(invCount & 1);
-	}
-	else     
-	{
-		int pos = findXPosition(puzzle);
-		if (pos & 1)
-			return !(invCount & 1);
-		else
-			return invCount & 1;
-	}
-}
-
-int main() 
-{
-    short field[N*N];
-
-    for (size_t i = 0; i < N*N; i++) 
-	{
-        std::cin >> field[i];
+    uint_fast32_t current_state = SOLVED;
+    while (current_state != start_field)
+    {
+        path.push_back(previos_states[current_state].second);
+        current_state = previos_states[current_state].first;
     }
 
-    if (!isSolvable(field)) 
-	{
-        std::cout << -1 << std::endl;
-        return 0;
+    std::reverse(path.begin(), path.end());
+
+    return path;
+}
+
+
+int main()
+{
+    Solver solver;
+
+    for (uint16_t i = 0; i < N * N; ++i)
+    {
+        uint16_t temp;
+        std::cin >> temp;
+        solver.setTile(i, temp);
     }
 
-    std::string solution = getSolution(field);
-    std::cout << solution.size()<< std::endl;
-	std::cout << solution << std::endl;
+    std::string res = solver.Solve();
+    if (res != "-1")
+    {
+        std::cout << res.length() << std::endl;
+    }
+    std::cout << res << std::endl;
 
     return 0;
 }
